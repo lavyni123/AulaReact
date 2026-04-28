53d3e7fc85ae09637a967e908dd2a9b25cc033c6
lavyni123 <lavynisantana019@gmail.com>
2026-04-28 11:36:38 -0300

    28/04/26

---
+++ aula02/src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import Exemplo1 from "./Exemplo1";
+export default function App() {
+  return (
+    <div>
+      <h1>Aula 02- Criação de Componentes e props
+      </h1>
+      <div className="card">
+        <h3>Chamdas para o componente Exemplo1</h3>
+<Exemplo1 numero1={10} numero2={20} />
+      </div>
+    </div>
+  )
+}
