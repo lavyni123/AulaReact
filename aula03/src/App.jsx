9ffa12a53f3f85ebc7ec2b7337b79af158427935
lavyni123 <lavynisantana019@gmail.com>
2026-08-11 10:19:37 -0300

    11/8

--- aula03/src/App.jsx
+++ aula03/src/App.jsx
@@ -1,4 +1,5 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from"react-router-dom";
+
 import Home from "./paginas/Home";
 import SaoPaulo from "./paginas/SaoPaulo";
 import MinasGerais from "./paginas/MinasGerais";
@@ -8,22 +9,25 @@ import EspiritoSanto from "./paginas/EspiritoSanto";
 
 export default function App()
 {
-  return (
-    <BrowserRouter>
-    <Routes>
+return(
+<BrowserRouter>
+
+<Routes>
 
 <Route path="/" element={<Home />} />
 
-<Route path="/estados/sp" element={<SaoPaulo />} />
+<Route path="/estados/sp" element={<SaoPaulo />}  />
+<Route path="/estados/mg" element={<MinasGerais />}  />
+<Route path="/estados/rj" element={<RioJaneiro />}  />
+<Route path="/estados/es" element={<EspiritoSanto />}  />
 
-<Route path="/estados/sp" element={<MinasGerais />} />
+</Routes>
 
-<Route path="/estados/sp" element={<RioJaneiro />} />
-
-<Route path="/estados/sp" element={<EspiritoSanto />} />
+</BrowserRouter>
 
 
-    </Routes>
-    </BrowserRouter>
-  )
+
+);
+
+
 }
